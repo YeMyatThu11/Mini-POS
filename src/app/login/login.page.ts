748111e305544c44992a718bb9fba4bbe63a05b5
Ye Myat Thu <scm.yemyatthu@gmail.com>
2020-11-23 17:08:00 +0630

    login form validation with formbuilder

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder,Validators,FormGroup} from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
+  private loginFormData: FormGroup;
+  get userId() {
+    return this.loginFormData.get("userId");
+  }
+  public errorMessages = {
+    userId: [
+      {
+        type: "required",
+        message: "Please Enter valid User ID",
+      },
+    ]
+  }
+  constructor(private FormBuilder:FormBuilder,private router:Router) { 
+    this.loginFormData=this.FormBuilder.group({
+      userId:["",[Validators.required,Validators.maxLength(20)]]
+    });
+  }
+  public submit(){
+    let userId=this.loginFormData.value.name;
+    if(this.validateUser(userId)){
+      this.router.navigate['home'];
+    }
+  }
   ngOnInit() {
+  }
+  validateUser(userId){ 
+    return true;
   }
   
 }
